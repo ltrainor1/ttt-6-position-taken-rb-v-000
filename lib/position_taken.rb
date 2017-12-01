07723e7a0ffd26699ae36202ad544e99a4d1f470
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-01 05:40:01 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here!\
+def position_taken?(board, index)
+  index = index.to_i
+  position = board[index]
+  a = position == "X"
+  b = position == "O"
+  if a||b
+    return true
+  else
+    return false
+  end
+end
